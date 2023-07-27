e55daf393946c8fcfb80295d83950088bd843b1c
Jun Zhang <junz93@outlook.com>
2023-07-28 07:38:29 +0800

    Update endpoints for characters

--- assistant/urls.py
+++ assistant/urls.py
@@ -4,9 +4,9 @@ from . import apis, views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('character/<int:id>', views.get_character, name='get_character'),
     path('character/create', views.create_character, name='create_character'),
     path('character/<int:id>/update', views.update_character, name='update_character'),
-    path('character/<int:id>/overview', views.character_overview, name='character_overview'),
     path('danmu_interaction', views.danmu_interaction, name='danmu_interaction'),
     path('api/answer', apis.get_answer),
 ]
--- assistant/views.py
+++ assistant/views.py
@@ -10,47 +10,51 @@ def index(request: HttpRequest):
 def danmu_interaction(request: HttpRequest):
     return render(request, 'assistant/danmu_interaction.html', {})
 
+@require_POST
 def create_character(request: HttpRequest):
-    if request.method == 'POST':
-        character_form = CharacterForm(request.POST)
+    # if request.method == 'POST':
+    character_form = CharacterForm(request.POST)
+    if character_form.is_valid():
+        character_form.save()
+        return HttpResponse('Succeeded!')
+    else:
+        return HttpResponseBadRequest('Invalid input')
+    # else:
+    #     character_form = CharacterForm()
+    #     return render(request, 'assistant/character.html', {'form': character_form})
+    
+@require_POST
+def update_character(request: HttpRequest, id: int):
+    try:
+        character = Character.objects.get(id=id)
+        # if request.method == 'POST':
+        character_form = CharacterForm(request.POST, instance=character)
         if character_form.is_valid():
             character_form.save()
             return HttpResponse('Succeeded!')
         else:
             return HttpResponseBadRequest('Invalid input')
-    else:
-        character_form = CharacterForm()
-        return render(request, 'assistant/character.html', {'form': character_form})
-    
-
-def update_character(request: HttpRequest, id: int):
-    try:
-        character = Character.objects.get(id=id)
-        if request.method == 'POST':
-            character_form = CharacterForm(request.POST, instance=character)
-            if character_form.is_valid():
-                character_form.save()
-                return HttpResponse('Succeeded!')
-            else:
-                return HttpResponseBadRequest('Invalid input')
-        else:
-            # character = Character.objects.get(id=id)
-            character_form = CharacterForm(instance=character)
-            return render(request, 'assistant/character.html', {'form': character_form})
+        # else:
+        #     # character = Character.objects.get(id=id)
+        #     character_form = CharacterForm(instance=character)
+        #     return render(request, 'assistant/character.html', {'form': character_form})
     except Character.DoesNotExist:
         raise Http404(f'Character with ID {id} does not exist')
+    
+@require_POST
+def delete_character(request: HttpRequest, id: int):
+    Character.objects.filter(id=id).delete()
+    return HttpResponse('Succeeded!')
 
-def character_overview(request: HttpRequest, id: int):
-    return HttpResponse('TODO')
-
-# def get_character(request: HttpRequest, id: int):
-#     try:
-#         character = Character.objects.get(id=id)
-#         character_dict = {
-#             'id': character.pk,
-#             'name': character.name,
-#             'gender': character.gender,
-#         }
-#         return JsonResponse(character, json_dumps_params={'ensure_ascii': False})
-#     except Character.DoesNotExist:
-#         raise Http404(f'Character with ID {id} does not exist')
+@require_GET
+def get_character(request: HttpRequest, id: int):
+    try:
+        character = Character.objects.get(id=id)
+        character_dict = {
+            'id': character.pk,
+            'name': character.name,
+            'gender': character.gender,
+        }
+        return JsonResponse(character, json_dumps_params={'ensure_ascii': False})
+    except Character.DoesNotExist:
+        raise Http404(f'Character with ID {id} does not exist')
